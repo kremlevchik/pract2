82e06d7f4353c5bec6dbf440955678322186c6d4
Ivan <kremlevchik98@mail.ru>
2024-02-04 21:48:38 +0300

    completed 4

---
+++ DZ4/DZ4.cs
@@ -0,0 +1,22 @@
+﻿//  Задача 4: Напишите программу, которая на вход принимает натуральное число N, а на выходе показывает его цифры через запятую.
+    Console.WriteLine("Введите натуральное число N: "); 
+int N = Convert.ToInt32(Console.ReadLine()); 
+        if (N <= 0) 
+        { 
+            Console.WriteLine("Число должно быть натуральным"); 
+        } 
+        else 
+        { 
+            string digits = ""; 
+ 
+            while (N > 0) 
+            { 
+                int digit = N % 10; 
+                digits = digit + ", " + digits; 
+                N /= 10; 
+            } 
+ 
+            digits = digits.Remove(digits.Length - 2); // Удаляем лишнюю запятую и пробел в конце 
+ 
+            Console.WriteLine($"Цифры числа через запятую: {digits}");
+        }
