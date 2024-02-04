7b155e21d00f2a7dd8cda9c9ed47accc46bf936d
Ivan <kremlevchik98@mail.ru>
2024-02-04 20:51:02 +0300

    solution 3

---
+++ DZ3/DZ3.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число от 10 до 99: ");
+int i = int.Parse(Console.ReadLine()!);
+while (i < 10 || i > 99){
+    Console.WriteLine("Вы ошиблись! \nВведите корректное число: ");
+    i = int.Parse(Console.ReadLine()!);
+ }
+ int number1 = i / 10;
+ int number2 = i % 10;
+    if (number1 > number2){
+        System.Console.WriteLine("Первое чило больше");
+    }
+    else
+    System.Console.WriteLine("Второе число больше");
+     
